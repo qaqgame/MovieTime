7526add15f46c2db563a59cd2cc21c7f62e7d5fd
glodxy <34620605+glodxy@users.noreply.github.com>
2020-07-04 11:07:19 +0800

    add recomm

---
+++ Recom/Utils.py
@@ -0,0 +1,40 @@
+
+from Recom.models import CosRelation
+
+def ImportRelation(id1,id2,relation):
+    movId1='Mov'+str(id1)
+    movId2='Mov'+str(id2)
+
+    # 如果存在则直接结束
+    if CosRelation.objects.filter(Movie1=movId1,Movie2=movId2).exists() or CosRelation.objects.filter(Movie2=movId1,Movie1=movId2).exists():
+        return
+
+    #否则添加
+    CosRelation.objects.create(Movie1=movId1,Movie2=movId2,Relation=relation)
+
+# 获得推荐电影列表：电影id，推荐数目
+def GetRecommList(id,count):
+    result=[]
+    tempList=[]
+    tempRecoms=CosRelation.objects.filter(Movie1=id).order_by('-Relation')
+    # 如果数量足够则直接取
+    if tempRecoms.count()>count:
+        for i in range(0,count):
+            result.append(tempRecoms[i].Movie2)
+        return result
+    else:
+        for temp in tempRecoms:
+            tempList.append(temp)
+
+    while len(result) <count and len(tempList)>0:
+        tempMov=tempList.pop()
+        # 不在则添加
+        if tempMov.Movie2 not in result:
+            result.append(temp.Movie2)
+        # 查询间接推荐
+        tempRecoms=CosRelation.objects.filter(Movie1=tempMov.Movie2).order_by('-Relation')
+        for temp in tempRecoms:
+            tempList.append(temp)
+
+    return result
+
